b0ba32a13f3ae2362d0a19346c4fcd18ea191da9
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-09-17 21:54:44 +0100

    making changes to graphql client var

--- pages/graphql/[detail].js
+++ pages/graphql/[detail].js
@@ -1,13 +1,39 @@
-import {query} from './home'
-import {request} from 'graphql-request'
+//import {query} from './home'
+import {request, gql} from 'graphql-request'
+
+const queryIdOnly = gql`
+
+    {
+        users{
+        data{
+            id
+        }
+        
+        }
+  }
+
+`
+
+const queryUserById = gql `
+  {
+    user(id: $id) {
+        id
+        email
+        name
+        phone
+        
+      }
+  }
+
+`
 
 export async function getStaticPaths(){
     const data = await request({
         url: 'https://graphqlzero.almansi.me/api',
-        document: query
+        document: queryIdOnly
     })
 
-    const paths = data.map(user =>{
+    const paths = data.users.data.map(user =>{
         return{
             params: {detail: user.id.toString()}
         }
@@ -15,14 +41,20 @@ export async function getStaticPaths(){
 
     return{
         paths,
-        fallback
+        fallback: false
     }
 }
 
-export async function getStaticProps(){
+export async function getStaticProps({params}){
+    const variables = {
+        id: params.detail
+    }
+   
     const data = await request({
         url: 'https://graphqlzero.almansi.me/api',
-        document: query
+        document: queryUserById,
+        variables: variables
+
     })
 
     return{
@@ -34,18 +66,20 @@ export async function getStaticProps(){
 
 
 export default function detail({data}) {
-    const {name, email, id, phone} = data
+    // const {name, email, id, phone} = data
+
+    console.log(data)
   return (
     <div>
 
         <div className='detail-container'>
-            <h3>Welcome to GraphQL detail page</h3>
+            {/* <h3>Welcome to GraphQL detail page</h3>
             <p>id: {id}</p>
             <p>name: {name}</p>
             <p>email: {email}</p>
-            <p>contact: {phone}</p>
+            <p>contact: {phone}</p> */}
         </div>
-        <Link href={'/rest/home'}>
+        <Link href={'/graphl/home'}>
             <button>
                 Home
             </button>
